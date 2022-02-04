db42e07a2ece948242871b906b242af3f7ea9dab
Filip Luac <oliveripepa1207>
2022-02-04 16:10:04 +0100

    fix(web): migrating to new firestore

--- client/projects/web/src/app/utils/app-init.ts
+++ client/projects/web/src/app/utils/app-init.ts
@@ -1,5 +1,5 @@
 import {Injector} from '@angular/core';
-import {AngularFirestore} from '@angular/fire/firestore';
+import {Firestore, doc, getDoc} from '@angular/fire/firestore';
 import {INITIAL_STATE} from '../consts/initial-state.const';
 import {PAGE_BLACK_LIST} from '../consts/page-black-list.const';
 
@@ -11,8 +11,7 @@ export async function appInit(injector: Injector) {
   }
 
   if (page && !PAGE_BLACK_LIST.includes(page)) {
-    const afs = injector.get(AngularFirestore);
-    const res = await afs.collection('pages').doc(page).get().toPromise();
+    const res = await getDoc(doc(injector.get(Firestore), 'pages', page));
 
     if (res.exists) {
       INITIAL_STATE.collections.pages = {
