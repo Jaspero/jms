e4dec69ffcf7b17770e6698e07f35878c631099b
AntonioStipic <antonio.stipic2@gmail.com>
2022-04-15 11:34:10 +0200

    fix(functions): remove firestore calls before initialization. redeploy storage trigger functions

--- functions/src/rest/proxy.ts
+++ functions/src/rest/proxy.ts
@@ -4,8 +4,6 @@ import {SHARED_CONFIG} from 'definitions';
 const cors_proxy = require('cors-anywhere');
 const server = cors_proxy.createServer({
   originWhitelist: []
-  // requireHeader: ['origin', 'x-requested-with'],
-  // removeHeaders: ['Content-Disposition']
 });
 export const proxy = functions
   .region(SHARED_CONFIG.cloudRegion)
