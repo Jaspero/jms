657d20391997ae9657091aeca4306fd25c117fe0
flauc <filip.lauc93@gmail.com>
2022-05-03 22:40:31 +0200

    feat: various translation updates

--- client/transloco.config.js
+++ client/transloco.config.js
@@ -1,6 +1,10 @@
 module.exports = {
 	scopedLibs: [
     {
+      src: './node_modules/@jaspero/fb-fields-mat',
+      dist: ['./projects/cms/src/assets/i18n/']
+    },
+    {
       src: './node_modules/@jaspero/jmsp-notes',
       dist: ['./projects/cms/src/assets/i18n/']
     }
