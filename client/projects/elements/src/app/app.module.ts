05755d4a1591e2ae61a73b6feeb54966901b9a4b
flauc <filip.lauc93@gmail.com>
2022-05-27 10:59:55 +0200

    feat(client): added elements project

---
+++ client/projects/elements/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
