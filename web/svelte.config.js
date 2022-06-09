b6a9be3ed2da853932cb698f97320f2a8134d60f
flauc <filip.lauc93@gmail.com>
2022-06-09 16:15:13 +0200

    feat: 404 page working; better cache config;

--- web/svelte.config.js
+++ web/svelte.config.js
@@ -14,7 +14,12 @@ const config = {
 			pages: '../public/web'
 		}),
 		prerender: {
-      default: true
+      default: true,
+			/**
+			 * You can add any pages that should be rendered
+			 * but aren't accessable through links on the website here.
+			 */
+			entries: ['/404']
     }
 	}
 };
