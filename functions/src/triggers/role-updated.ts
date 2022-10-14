cc5673d3f3105393af83e5513966882a0ba8311a
AntonioStipic <antonio.stipic2@gmail.com>
2022-10-14 17:23:04 +0200

    fix(functions): role updated detect changes

--- functions/src/triggers/role-updated.ts
+++ functions/src/triggers/role-updated.ts
@@ -3,50 +3,56 @@ import {auth, firestore} from 'firebase-admin';
 import * as functions from 'firebase-functions';
 
 export const roleUpdated = functions
-	.region(SHARED_CONFIG.cloudRegion)
-	.firestore
-	.document(`${Collections.Roles}/{documentId}`)
-	.onUpdate(async change => {
-		const after: any = change.after.data();
-		const before: any = change.before.data();
+  .region(SHARED_CONFIG.cloudRegion)
+  .firestore
+  .document(`${Collections.Roles}/{documentId}`)
+  .onUpdate(async change => {
+    const after: any = change.after.data();
+    const before: any = change.before.data();
 
-		const ah = auth();
-		const fs = firestore();
-		const keys = ['get', 'list', 'create', 'update', 'delete'];
+    const ah = auth();
+    const fs = firestore();
+    const keys = ['get', 'list', 'create', 'update', 'delete'];
 
-		const diff = (() => {
-			for (const key in after.permissions) {
-				if (!before.hasOwnProperty(key)) {
+    const permissionsKeys = new Set<string>();
+    for (const key of Object.keys(after.permissions)) {
+      permissionsKeys.add(key);
+    }
+
+    for (const key of Object.keys(before.permissions)) {
+      permissionsKeys.add(key);
+    }
+
+		const diff = Array.from(permissionsKeys).some((key) => {
+			if (before[key] === undefined) {
+				return true;
+			}
+
+			for (const k of keys) {
+				if (after.permissions?.[key]?.[k] !== before.permissions?.[key]?.[k]) {
 					return true;
-				}
-
-				for (const k of keys) {
-					if (after.permissions[key][k] !== before.permissions[key][k]) {
-						return true;
-					}
 				}
 			}
 
 			return false;
-		})();
+		});
 
-		if (diff) {
-			const {docs} = await fs.collection(Collections.Users)
-				.where('role', '==', change.after.id)
-				.get();
+    if (diff) {
+      const {docs} = await fs.collection(Collections.Users)
+        .where('role', '==', change.after.id)
+        .get();
 
-			await Promise.allSettled(
-				docs.map(doc =>
-					ah.setCustomUserClaims(
-						doc.id,
-						{
-							permissions: after.permissions,
-							role: change.after.id
-						}
-					)
-				)
-			)
-		}
-
-	});
+      await Promise.allSettled(
+        docs.map(doc =>
+          ah.setCustomUserClaims(
+            doc.id,
+            {
+              permissions: after.permissions,
+              role: change.after.id
+            }
+          )
+        )
+      );
+    }
+  });
 
