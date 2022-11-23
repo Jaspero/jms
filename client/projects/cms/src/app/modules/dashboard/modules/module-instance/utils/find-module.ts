a3b00b753d4fd100fa7c4dc3a8157b95b64f08b4
flauc <filip.lauc93@gmail.com>
2022-11-23 18:44:18 +0100

    fix: finding child module when main is requested

--- client/projects/cms/src/app/modules/dashboard/modules/module-instance/utils/find-module.ts
+++ client/projects/cms/src/app/modules/dashboard/modules/module-instance/utils/find-module.ts
@@ -14,5 +14,10 @@ export function findModule(
     return acc;
   }, []);
 
-  return modules.find(mod => !moduleIds.some(id => !mod.id.includes(id)));
+  const module = modules.find(mod =>
+    !moduleIds.some(id => !mod.id.includes(id)) &&
+    Math.ceil(mod.id.split('/').filter(Boolean).length / 2) === moduleIds.length
+  );
+
+  return module;
 }
