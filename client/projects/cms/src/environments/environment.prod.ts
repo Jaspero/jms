54fcedf7164658a7884fb6677ccd48b484c8a92a
flauc <filip.lauc93@gmail.com>
2023-01-09 21:41:02 +0100

    feat(client): updating to angular v15

--- client/projects/cms/src/environments/environment.prod.ts
+++ client/projects/cms/src/environments/environment.prod.ts
@@ -3,7 +3,6 @@ import {ENV_CONFIG} from '../../../../env-config';
 export const environment = {
   origin: '',
   production: true,
-  timeStamp: '6/27/2021 - 8:55:00 PM',
   firebase: ENV_CONFIG.firebase,
   firebaseEmulators: false
 };
