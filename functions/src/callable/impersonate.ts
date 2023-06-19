1d5ae8e3b7b9af6f5e1eb193b712f2d10f6ffa71
flauc <filip.lauc93@gmail.com>
2023-06-19 16:00:36 +0200

    fix(functions): only admins can impersonate other users

--- functions/src/callable/impersonate.ts
+++ functions/src/callable/impersonate.ts
@@ -1,13 +1,13 @@
-import {Collections, SHARED_CONFIG} from 'definitions';
+import {SHARED_CONFIG} from 'definitions';
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
-import {hasPermission} from '../utils/auth';
+import {hasRole} from '../utils/auth';
 
 export const impersonate = functions
   .region(SHARED_CONFIG.cloudRegion)
   .https
   .onCall(async (data, context) => {
-    await hasPermission(context, Collections.Users, 'list');
+    await hasRole(context, 'admin');
 
     const auth = admin.auth();
     const user = await auth.getUser(data);
